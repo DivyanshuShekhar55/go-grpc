2f08555fa6486cf24e831c6623e0bff0ae6a0ff3
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-16 09:52:07 +0530

    added signatures for orderService

--- services/orders/service/orders.go
+++ services/orders/service/orders.go
@@ -1,1 +1,14 @@
 package service
+
+import "github.com/DivyanshuShekhar55/go-grpc/services/common/genproto/orders"
+
+// create a mock db
+var ordersDB = make([]*orders.Order, 0)
+
+type OrderService struct {
+	// store implementation
+}
+
+func NewOrderService() *OrderService {
+	return &OrderService{}
+}
