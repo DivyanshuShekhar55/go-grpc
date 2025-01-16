44636dd9c9b6d9a2a4b89ffc203eeb2b6355b758
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-16 08:03:39 +0530

    added the run function for grpc server

--- services/orders/grpc.go
+++ services/orders/grpc.go
@@ -1,9 +1,36 @@
 package main
 
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
 type gRPCServer struct {
 	addr string
 }
 
-func NewGRPCServer(addr string) *gRPCServer{
-	return &gRPCServer{addr : addr}
+func NewGRPCServer(addr string) *gRPCServer {
+	return &gRPCServer{addr: addr}
+}
+
+func (s *gRPCServer) Run() error {
+
+	// listen to a tcp server, via listener
+	lis, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		log.Fatalf("failed to listen %v", err)
+	}
+
+	// create the server
+	grpcServer := grpc.NewServer()
+
+	// register our gRPC services
+	// TODO
+
+	// make the grpc server listen to tcp listener ...
+	log.Println("starting grpc server on", s.addr)
+
+	return grpcServer.Serve(lis)
 }
