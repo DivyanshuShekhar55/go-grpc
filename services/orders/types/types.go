4d1a56a0f63b4838abe556ec0085fd4716198e2e
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-16 08:35:34 +0530

    added order-service interface and updated instructions.txt file

--- services/orders/types/types.go
+++ services/orders/types/types.go
@@ -1,1 +1,10 @@
 package types
+
+import (
+	"context"
+	"github.com/DivyanshuShekhar55/go-grpc/services/common/genproto/orders"
+)
+
+type OrderService interface {
+	CreateOrder(context.Context, *orders.Order) error
+}
