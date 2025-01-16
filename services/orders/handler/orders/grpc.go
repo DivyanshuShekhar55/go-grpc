e561b9e8a3535f7991f15aade87631bf74fb0bb1
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-16 09:24:05 +0530

    implemented the createOrder service

--- services/orders/handler/orders/grpc.go
+++ services/orders/handler/orders/grpc.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"context"
+
 	"github.com/DivyanshuShekhar55/go-grpc/services/common/genproto/orders"
 	"github.com/DivyanshuShekhar55/go-grpc/services/orders/types"
 	"google.golang.org/grpc"
@@ -9,7 +11,7 @@ import (
 type OrdersGrpcHandler struct {
 	// the orderService is an interface, so basically saying our handler will have this, this and those functions
 	ordersService types.OrderService
-	
+
 	// this is the fallback or default created in auto-generation code
 	orders.UnimplementedOrderServiceServer
 }
@@ -25,5 +27,32 @@ func NewGrpcOrdersService(grpc *grpc.Server, ordersService types.OrderService) {
 	// then we "register" this handler with the main grpc server (services/orders/grpc.go)
 	// we can have another handler for another thing and also register that to the same server
 	orders.RegisterOrderServiceServer(grpc, gRPCHandler)
+
+}
+
+// the actual service implementation, a fallback was created in the common folder orders.grpc.pb.go
+
+func (h *OrdersGrpcHandler) CreateOrder(ctx context.Context, req *orders.CreateOrderRequest) (*orders.CreateOrderResponse, error) {
+
+	// create a mock order request
+	order := &orders.Order{
+		OrderID:    42,
+		CustomerId: 2,
+		ProductID:  1,
+		Quantity:   10,
+	}
+
+	// call the createOrder function
+	err := h.ordersService.CreateOrder(ctx, order)
+	if err != nil {
+		return nil, err
+	}
+
+	// if no err create the response
+	res := &orders.CreateOrderResponse{
+		Status: "success",
+	}
+
+	return res, nil
 
 }
