7a457b42f9dabc50af4e5d6fc67f3cdad28452f5
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-17 14:17:41 +0530

    added code for the main http.go file

--- services/orders/http.go
+++ services/orders/http.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	handler "github.com/DivyanshuShekhar55/go-grpc/services/orders/handler/orders"
+	"github.com/DivyanshuShekhar55/go-grpc/services/orders/service"
+)
+
 // this is the place where we can handle the http services / routes ... just fopr example purposes
 // we are using just the net/http lib but you might want to use chi (or smthing else) too
 
@@ -10,4 +18,21 @@ type httpServer struct {
 func NewHttpServer(addr string) *httpServer {
 	srv := &httpServer{addr: addr}
 	return srv
+}
+
+func (s *httpServer) Run() error {
+
+	// create a router
+	r := http.NewServeMux()
+
+	// register all the http routing services
+	// the NewOrderService will ideally be a interface of func handlers
+	orderService := service.NewOrderService()
+	orderHandler := handler.NewHttpOrderHandler(orderService)
+
+	// following will add all routes of http to this main mux 'r'
+	orderHandler.RegisterRouter(r)
+
+	log.Println("Starting Server On", s.addr)
+	return http.ListenAndServe(s.addr, r)
 }
