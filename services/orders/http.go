bf5f258390d48f039574bfbe113b7773aacaaebe
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-17 08:29:43 +0530

    added struct and constructor for http server

--- services/orders/http.go
+++ services/orders/http.go
@@ -1,3 +1,13 @@
 package main
 
 // this is the place where we can handle the http services / routes ... just fopr example purposes
+// we are using just the net/http lib but you might want to use chi (or smthing else) too
+
+type httpServer struct {
+	addr string
+}
+
+func NewHttpServer(addr string) *httpServer {
+	srv := &httpServer{addr: addr}
+	return srv
+}
