7ad9d047b861b79ff69f25bc049c889944bf0c5d
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-17 14:14:24 +0530

    added the http file for orders

--- services/orders/handler/orders/http.go
+++ services/orders/handler/orders/http.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"net/http"
 
+	"github.com/DivyanshuShekhar55/go-grpc/services/common/genproto/orders"
+	"github.com/DivyanshuShekhar55/go-grpc/services/common/util"
 	"github.com/DivyanshuShekhar55/go-grpc/services/orders/types"
 )
 
@@ -18,10 +20,40 @@ func NewHttpOrderHandler(orderService types.OrderService) *OrdersHttpHandler {
 	return handler
 }
 
-func(h *OrdersHttpHandler) RegisterRouter (router *http.ServeMux){
-	router.HandleFunc("POST /orders",h.CreateOrder)
+func (h *OrdersHttpHandler) RegisterRouter(router *http.ServeMux) {
+	router.HandleFunc("POST /orders", h.CreateOrder)
 }
 
-func (h *OrdersHttpHandler) CreateOrder(w http.ResponseWriter, r *http.Request){
-	
+func (h *OrdersHttpHandler) CreateOrder(w http.ResponseWriter, r *http.Request) {
+
+	//get req thing
+	var req orders.CreateOrderRequest
+	//parse json
+	err := util.ParseJSON(r, &req)
+	if err != nil {
+		util.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// create a mock order ...
+	// in the proto file we created a structure that had customerid, productid, quantity
+	// in protofile generated we have methods to get these fields from the req, orders.pb.go line 60
+	order := &orders.Order{
+		OrderID:    42,
+		CustomerId: req.GetCustomerID(),
+		ProductID:  req.GetProductID(),
+		Quantity:   req.GetQuantity(),
+	}
+
+	// follwoing is the grpc handler, not the http one
+	err = h.ordersService.CreateOrder(r.Context(), order)
+	if err != nil {
+		util.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	// all good, return the protobuf response and the http response
+	res := &orders.CreateOrderResponse{Status: "success"}
+	util.WriteJSON(w, http.StatusOK, res)
+
 }
