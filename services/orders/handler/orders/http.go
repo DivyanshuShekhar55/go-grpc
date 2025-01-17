7c599ead020240377fa01473bc73a69ff18e995a
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-17 09:14:53 +0530

    added handler constructor and struct

--- services/orders/handler/orders/http.go
+++ services/orders/handler/orders/http.go
@@ -1,2 +1,15 @@
 package handler
 
+import "github.com/DivyanshuShekhar55/go-grpc/services/orders/types"
+
+type OrdersHttpHandler struct {
+	ordersService types.OrderService
+}
+
+func NewHttpOrderHandler(orderService types.OrderService) *OrdersHttpHandler {
+	handler := &OrdersHttpHandler{
+		ordersService: orderService,
+	}
+
+	return handler
+}
