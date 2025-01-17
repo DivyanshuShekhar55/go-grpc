49673a63ad535edc751da9161167a3cbf1edf95b
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-17 09:44:06 +0530

    added route register func

--- services/orders/handler/orders/http.go
+++ services/orders/handler/orders/http.go
@@ -1,6 +1,10 @@
 package handler
 
-import "github.com/DivyanshuShekhar55/go-grpc/services/orders/types"
+import (
+	"net/http"
+
+	"github.com/DivyanshuShekhar55/go-grpc/services/orders/types"
+)
 
 type OrdersHttpHandler struct {
 	ordersService types.OrderService
@@ -12,4 +16,12 @@ func NewHttpOrderHandler(orderService types.OrderService) *OrdersHttpHandler {
 	}
 
 	return handler
+}
+
+func(h *OrdersHttpHandler) RegisterRouter (router *http.ServeMux){
+	router.HandleFunc("POST /orders",h.CreateOrder)
+}
+
+func (h *OrdersHttpHandler) CreateOrder(w http.ResponseWriter, r *http.Request){
+	
 }
