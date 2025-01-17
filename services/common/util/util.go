55adc749b4c4db9d160aaaa9e15055cbaaa70ac6
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-17 13:38:38 +0530

    added json parsing function

--- services/common/util/util.go
+++ services/common/util/util.go
@@ -1,1 +1,15 @@
 package util
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func ParseJSON(r *http.Request, v any) error{
+	if r.Body == nil {
+		return fmt.Errorf("missing req body")
+	}
+
+	return json.NewDecoder(r.Body).Decode(v)
+}
