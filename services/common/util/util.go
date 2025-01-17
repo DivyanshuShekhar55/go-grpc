2b9941fec64ecd2145c8ab716ce710aaf7b9a9de
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-17 13:41:54 +0530

    added json-writer and error-json-writer

--- services/common/util/util.go
+++ services/common/util/util.go
@@ -6,7 +6,17 @@ import (
 	"net/http"
 )
 
-func ParseJSON(r *http.Request, v any) error{
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+func WriteError(w http.ResponseWriter, status int, err error) {
+	WriteJSON(w, status, map[string]string{"error": err.Error()})
+}
+
+func ParseJSON(r *http.Request, v any) error {
 	if r.Body == nil {
 		return fmt.Errorf("missing req body")
 	}
