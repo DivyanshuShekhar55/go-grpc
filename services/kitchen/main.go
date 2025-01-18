61998e293ca902f6db1e2d8888fa0272d32579a1
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-18 11:04:50 +0530

    added the client code for grpc server

--- services/kitchen/main.go
+++ services/kitchen/main.go
@@ -1,1 +1,18 @@
-package main 
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// the kitchen acts as a client for grpc services, requesting response and sending request
+func NewGRPCClient(addr string) *grpc.ClientConn {
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect %v", err)
+	}
+
+	return conn
+}
